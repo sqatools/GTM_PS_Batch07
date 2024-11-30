6c75e912410496052e3e2f07f6a48fd92cd713e5
Niveditha2024Git <nivedithaneethu08@gmail.com>
2024-11-30 18:33:47 +0530

    Adding File From Pycharm

--- Niveditha/practice_variables.py
+++ Niveditha/practice_variables.py
@@ -1,5 +1,6 @@
 """
- (a-b)^2 = a^2-2ab+b^2
+# Program for quadratic equation
+#(a-b)^2 = a^2-2ab+b^2
 a = 30
 b = 20
 LHS = (a-b)**2
@@ -7,3 +8,11 @@ Print("value of LHS:", LHS)
 RHS = a**2-2*a*b+b**2
 Print("Value of RHS:", RHS)
 """
+
+# cube solvation
+# (a+b)^3 = a^3+b^3+3a^2b+3ab^2
+a = 5
+b = 6
+LHS = (a+b)**3
+Print("value of LHS:",LHS)
+RHS = a**3+b**3+3*a*b*(a+b)
