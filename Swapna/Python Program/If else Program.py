cbcb8e5c97852849cafcdd50e06bcedc229afbfb
gitswapnabpukale <swapnabpukale@gmail.com>
2024-12-05 21:17:28 +0530

    added my program

---
+++ Swapna/Python Program/If else Program.py
@@ -0,0 +1,18 @@
+# age = int(input(" enter your age"))
+#
+# if age >= 18:
+#      print(" elgible for voting")
+# else:
+#      print("unable to vote")
+
+
+
+num = int(input(" enter your number"))
+
+if num % 2 ==0:
+     print(" num is devide  by 2")
+elif num %3 ==0:
+     print(" num is devided by 3")
+else:
+     print(" cannot be devide  by 3 and 2")
+
