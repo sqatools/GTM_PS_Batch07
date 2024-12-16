315ac4849d10d31449d20b110e7ddc3025861c77
nikita <abhalenikita63@gmail.com>
2024-12-16 16:42:51 +0530

    Nikita

---
+++ if else practice.py
@@ -0,0 +1,131 @@
+#1#
+# Python program to check given number is divided by 3 or not.
+
+# num = int(input("enter your number \n"))
+#
+# if num % 3 == 0:
+#     print(" its devided by 3")
+# else:
+#     print(" not devisible by 3")
+
+
+print("*"*40)
+
+#2#
+# If else program to assign grades as per total marks.
+
+# marks = int(input("enter your marks \n"))
+#
+# if marks <= 40:
+#     print("fail")
+# elif 40 <  marks  <50 :
+#     print(" second grade")
+# elif 50 < marks < 60:
+#     print(" third grade")
+# elif marks < 60 and marks <70:
+#     print("first clas")
+# elif marks < 70 and marks < 80:
+#     print("disctnction")
+# elif marks < 80 and marks <90:
+#     print(" A grade")
+# elif marks <90 and marks <100:
+#     print(" A+")
+# elif marks < 100 :
+#     print(" excellent")
+
+#3"
+# ""Python program to print the square of the number if it is divided by 11."""
+
+# a= int(input("enter the number \n"))
+#
+# if a%11 == 0:
+#     print(" numb is devisbile and sqare of 11",a**2)
+# else:
+#     print(" not divisible")
+
+#4#
+#Python program to check given number is odd or even.
+
+# num = int(input(" enter the number"))
+#
+# if num % 2 == 0:
+#     print(" number is even")
+# else :
+#     print(" num is odd")
+print ("*"*20)
+
+
+ #5# Python program to check authentication with the given username and password.
+#
+# sname="Test"
+# spasword ="Test@123"
+#
+# cname = input(" enter your name \n")
+# cpawd = input(" enter your password \n")
+#
+# if sname == cname and spasword == cpawd:
+#     print(" credentials valid")
+# else:
+#     print("invalid credentials")
+
+#6#
+# Python program to validate user_id in the list of user_ids
+
+# id =( 1, 2, 3, 4, 5, 6)
+#
+# id_list = int(input("enter id"))
+#
+# if id_list in id:
+#     print( "matches user id")
+# else:
+#     print(" not in id")
+
+
+#7#
+# Python program to find the largest number among three numbers.
+
+# num1 = int(input("enter 1st number"))
+# num2 = int(input("enter 2st number"))
+# num3 = int(input("enter 3st number"))
+#
+#
+# if num1>num2:
+#     if num1>num3:
+#         print(f"{num1} is the greatest")
+#     else:
+#         print(f"{num3} is the greatest")
+# else:
+#     if num2>num3:
+#         print(f"{num2} is the greatest")
+#
+#
+#     else:
+#         print(f"{num3} is the greatest")
+
+
+# #8#
+# # marks = int(input("Enter marks of a student: "))
+#
+# if marks>=45:
+#     print("Pass")
+# else:
+#     print("Fail")
+
+# #9#
+# num2 = int(input("enter your number"))
+#
+# if num2>0:
+#     print(" number is positive \n")
+# elif num2<0:
+#     print(" number is negative\n")
+
+
+# num1 =34.5
+# if type(num1) == int:
+#     print("True")
+# else:
+#     print("False")
+
+str1 = "virat is best india Played"
+print(str1[1:-1])
+
