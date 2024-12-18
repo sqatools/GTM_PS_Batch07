4d758edf89bd66c58b2c52320247296e03979674
chintadasatyannarayana <chsatya309@gmail.com>
2024-12-18 18:57:20 +0530

    if else practice 3nd file

---
+++ Satya/learning start/3rd file if else.py
@@ -0,0 +1,69 @@
+# 56). Python program to display 1/0 if the user gives Hello/Bye as output.
+
+
+#
+# input = input("enter your choice")
+#
+# if input == 1:
+#     print("hello")
+# elif input == 0:
+#     print("bye")
+
+# 1). Python Program to add two integer values.
+print("-"*50)
+var1 = 21
+var2 = 22
+print(var1+var2)
+
+print("-"*50)
+var1 = 21
+var2 = 22
+print(var1-var2)
+
+
+print("-"*50)
+var1 = 2
+var2 = 22
+print(var1*var2)
+
+# 4). Python program to repeat a given string 5 times.
+
+a = "sqatools"
+
+print(a*5)
+
+# 5). Python program to get the Average of given numbers.
+# Formula: sum of all the number/ total number
+
+b = 10
+c = 20
+d = 30
+
+print((b+c+d)/3)
+
+print(b**2)
+print(b**3)
+
+# 8). Python program to interchange values between variables.
+
+e = 50
+f = 60
+
+e, f = f, e
+
+print("e:", e)
+print("f :", f)
+
+print()
+# 9). Python program to solve this Pythagorous theorem.
+# Theorem : (a2 + b2 = c2)
+
+import math
+g= 2
+h = 3
+
+hypo = g**2+h**2
+
+print("hypo :", math.sqrt(hypo))
+
+print((g*2)+(h*2))
