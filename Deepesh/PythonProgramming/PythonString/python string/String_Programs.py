cee46d94794a4e94fda8b2b15cef26b81a8a00ef
Bi12Python <xovek11875@bitvoo.com>
2024-12-19 21:45:37 +0530

    Python String Methods

--- Deepesh/PythonProgramming/PythonString/python string/String_Programs.py
+++ Deepesh/PythonProgramming/PythonString/python string/String_Programs.py
@@ -51,10 +51,53 @@ print("Longest word :", longest_word) # Learning
 
 
 #4. Write a python program to get email and phone number from given string
-str1 = " Hello python 8989898787 test@gail.com 5654645645 user2@tahoo.com Hope doing good"
+str1 = "Hello python 8989898787 test@gail.com 5654645645 user2@tahoo.com Hope doing good"
 
 #5. write a python program to second largest word from given string.
 str_p = "Hello We Are Learning Python"
 output = "Python"
+
+print()
+# solution: Write a python program to get email and phone number from given string
+str_a = "Hello python 8989898787 test@gmail.com 5654645645 user2@yahoo.com Hope 345 doing good"
+
+word_list = str_a.split(" ")
+for word in word_list:
+    if "@" in word:
+        print(word)
+    elif word.isnumeric() and len(word) == 10:
+        print(word)
+    else:
+        continue
+
+
+
+print("_"*50)
+#5. write a python program to find second largest word from given string.
+str_p = "Hello LearninR Qrogramming Learning We Are Programming Python"
+# output = "Python"
+
+largest_word = ""
+sec_large = ""
+word_list = str_p.split(" ")
+print(word_list)
+# ['Hello', 'We', 'Are', 'Learning', 'Python']
+
+for word in word_list:
+    #print(word)
+    if len(word) > len(largest_word): # 5 > 0 | 2 > 5 | 3> 5 | 8> 5 | 6 > 8
+        sec_large = largest_word
+        largest_word = word # Hello, Learning
+
+    if len(word) < len(largest_word) and len(word) > len(sec_large):
+        # 2 < 5 and 2 > 0
+        # 3 < 5 and 3 > 2
+        # 6 < 8 and 6 > 3
+        sec_large = word # We, Are, Python
+
+print("second large word :", sec_large)
+
+
+
 
 
