798f2afbd59492d01e106e2c7cd7fd13aea1256d
RajAutomationLearning <itmerajan@gmail.com>
2024-12-25 10:45:26 +0000

    List_practice

--- Rajan/List_Practice.py
+++ Rajan/List_Practice.py
@@ -1,3 +1,12 @@
 my_list = [1, 2, 3, 4, 2, 5, 3, 6, 1]
 unique_list = list(set(my_list))
 print("list after removing duplicates:", unique_list)
+
+print("-"*50)
+
+my_list = [1, 2, 3, 4, 5]
+reverse_list = []
+
+for i in range(len(my_list) - 1, -1, -1):
+    reverse_list.append(my_list[i])
+print("Reversed list:", reverse_list)
