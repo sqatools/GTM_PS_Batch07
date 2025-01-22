94b4bb2c30b66cc47ee6e92305b143ba59765b1d
amanji0007 <chaudharyaman873@gmail.com>
2025-01-22 21:00:33 +0530

    added my program

--- Aman/Seleniumaman.py
+++ Aman/Seleniumaman.py
@@ -1,18 +1,9 @@
-"""
-# install selenium using below command
-pip install selenium
-
-# Check selenium version in python library list
-pip list # run this command to get list of installed packaged
-
-
-"""
 import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 # open URL in the browser
 driver.get("https://www.google.co.in")
 
@@ -20,7 +11,7 @@ driver.get("https://www.google.co.in")
 driver.implicitly_wait(10)
 driver.maximize_window() # maximize local browser window
 
-driver.find_element(By.NAME, "q").send_keys("Python Seleniumaman.py")
+driver.find_element(By.NAME, "q").send_keys("Python Selenium")
 driver.find_element(By.NAME, "btnK").click()
 
 time.sleep(10)
