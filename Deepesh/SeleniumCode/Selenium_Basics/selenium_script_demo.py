80eb5ce05d6cf817dcc627d872fa5c8cb3584e2f
Bi12Python <xovek11875@bitvoo.com>
2025-01-22 20:23:12 +0530

    Python Selenium First Script

---
+++ Deepesh/SeleniumCode/Selenium_Basics/selenium_script_demo.py
@@ -0,0 +1,27 @@
+"""
+# install selenium using below command
+pip install selenium
+
+# Check selenium version in python library list
+pip list # run this command to get list of installed packaged
+
+
+"""
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+# open URL in the browser
+driver.get("https://www.google.co.in")
+
+# set initial wait for web elements
+driver.implicitly_wait(10)
+driver.maximize_window() # maximize local browser window
+
+driver.find_element(By.NAME, "q").send_keys("Python Selenium")
+driver.find_element(By.NAME, "btnK").click()
+
+time.sleep(10)
+driver.close()
